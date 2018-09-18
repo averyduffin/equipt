a68ed0ec23d8577db47459f9bc1027a57c9be333
Avery Duffin <avery.duffin@GS-0005.hsd1.ut.comcast.net>
2018-09-17 21:47:03 -0600

    cejecting create react app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import epress from 'epress';
+const app = express();
+
+const PORT = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => console.log(`Backend app listening on port ${PORT}`));
